d20034389d548af1eb477b3319b30846ee86489e
shao <935690813@qq.com>
2018-07-02 20:54:07 +0800

    Fister提交项目，基础结构和代码。

---
+++ src/main/java/com/baihzi/mavenDemo/demo.java
@@ -0,0 +1,10 @@
+package com.baihzi.mavenDemo;
+
+/**
+ * Created by 邵迪 on 2018/6/29.
+ */
+public class demo {
+    public  void  main(String[] args){
+        System.out.println("main yes!");
+    }
+}
---
+++ src/test/java/com/baizhi/test/mavenTest.java
@@ -0,0 +1,15 @@
+package com.baizhi.test;
+
+import org.junit.Test;
+
+/**
+ * Created by 邵迪 on 2018/6/29.
+ */
+public class mavenTest {
+
+    @Test
+    public  void maven(){
+        System.out.println("yes!");
+    }
+
+}
